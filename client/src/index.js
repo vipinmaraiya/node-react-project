7389f4f9beb7ac562c012ab5454807aaafe954cc
Vipin Sharma <vipinsharmaec@hotmail.com>
2018-05-25 00:45:15 +0530

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({
+    async (accessToken, refreshToken, profile, done) => {
+      let existingUser = await User.findOne({
         googleId: profile.id
-      }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id }).save().then(user => {
-            done(null, user);
-          });
-        }
       });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      let user = await new User({ googleId: profile.id }).save();
+      return done(null, user);
     }
   )
 );
