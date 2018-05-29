486f9e4bdd404da600c7bfb605a8467f5e8085a0
Vipin Sharma <vipinsharmaec@hotmail.com>
2018-05-30 01:12:57 +0530

    Build commit

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "209424350896-k4gtf06cfojgnb167b8u29r493sctu6q.apps.googleusercontent.com",
+  googleClientSecret: "2A6qCQ2k_2603aVEBpPsqeW9",
+  mongoURI:
+    "mongodb://vipinsh:vipinsh@ds117719.mlab.com:17719/react-app-db-dev",
+  cookieKey: "vipinsh-dev",
+  stripePublishableKey: "pk_test_zGHQlJsU9dT4q2d0Lua2ZQo6",
+  stripeSecretKey: "sk_test_CCAhwdNmV8WY0sgAI5Txwt4c"
+};
