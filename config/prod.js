cc30d9c89af3d7312878dbff9794fbaa5dc866c3
Vipin Sharma <vipinsharmaec@hotmail.com>
2018-05-30 01:10:15 +0530

    Build commit

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,10 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  googleClientID:
+    "209424350896-k4gtf06cfojgnb167b8u29r493sctu6q.apps.googleusercontent.com",
+  googleClientSecret: "2A6qCQ2k_2603aVEBpPsqeW9",
+  mongoURI:
+    "mongodb://vipinsh:vipinsh@ds117719.mlab.com:17719/react-app-db-dev",
+  cookieKey: "vipinsh-dev",
+  stripePublishableKey: "pk_test_zGHQlJsU9dT4q2d0Lua2ZQo6",
+  stripeSecretKey: "sk_test_CCAhwdNmV8WY0sgAI5Txwt4c"
 };
